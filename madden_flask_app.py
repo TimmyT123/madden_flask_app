6d1e76f98dbc4e26f896b1317ece42cea976fbb6
TimmyT123 <timmyt123@hotmail.com>
2025-12-27 14:19:51 -0700

    - Fixed process_webhook_data for rosters #10

--- madden_flask_app.py
+++ madden_flask_app.py
@@ -1062,7 +1062,7 @@ def home():
         latest_week_display = "?"
         current_week = 0
 
-    print("DEBUG leagues:", leagues, flush=True)
+    #print("DEBUG leagues:", leagues, flush=True)
     print("DEBUG latest:", latest_league_id, latest_season, latest_week, flush=True)
 
     return render_template(
